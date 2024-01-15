613962c466696e948bf4ec0b124fa85d2ade29b3
Thoufiq <80845494+Thoufiq2705@users.noreply.github.com>
2024-01-15 22:11:10 +0530

    Update Resume.jsx
    Resume link updated

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -9,7 +9,7 @@ const Resume = () => {
         <div className="rcontent">
             <h2>Download <span className='span'>Resume</span></h2>
             <p>Welcome to my resume download section. Here, you can find detailed information about my qualifications and professional experience. Feel free to download my resume to learn more about my skills and expertise.</p>
-            <a href="https://drive.google.com/file/d/1BWN5ipHyrvLfFBrAVqLy1CFS6zu3m-VK/view?usp=sharing" target="_blank"><button className='download'>Download</button></a>
+            <a href="https://drive.google.com/file/d/1hzPpLz1DVvtdshnls0Eqcq0M5MGS5W6S/view?usp=sharing" target="_blank"><button className='download'>Download</button></a>
         </div>
         <div className="rimg">
             <img src={resume} alt="" />
